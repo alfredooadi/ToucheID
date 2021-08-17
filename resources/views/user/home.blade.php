cb75317109559512b087d8b9054c548f41cde22a
alfredooadiyasa <alfredo@student.umn.ac.id>
2021-08-17 13:37:33 +0700

    first commit

---
+++ resources/views/user/home.blade.php
@@ -0,0 +1,162 @@
+@extends('layouts.user')
+
+@section('header')
+    <style>
+        .full-img {
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            height: 180px;
+        }
+        #hero{
+            background: url('{{asset('user/images/background.jpg')}}') top center;
+        }
+        .image-center{
+          display: block;
+          margin-left: 6.5px;
+          margin-right: 6.5px;
+          width: 100%;
+        } 
+    </style>    
+@endsection
+
+@section('hero')
+    <h1>TOUCHE Development Center</h1>
+    <h2>Touching People</h2>
+    <a href="/contact" class="btn-get-started">Contact Us</a>
+@endsection
+
+
+@section('content')
+
+      <!--========================== About Us Section ============================-->
+      <section id="about">
+        <div class="container">
+          <div class="row about-container">
+            @if(!empty($about))
+            <div class="col-lg-7 content order-lg-1 order-2">
+              <h2 class="title">Tentang Kami</h2>
+              <p> {!!$about[0]->caption!!}</p>
+            </div>
+  
+            <div class="col-lg-5 background order-lg-2 order-1 wow fadeInRight" 
+                style="background: url('{{asset('about_image/'.$about[0]->image)}}') center top no-repeat; background-size: cover;"></div>
+          </div>
+            @endif
+        </div>
+      </section>
+  
+      <!--========================== Services Section ============================-->
+      <section id="services">
+        <div class="container wow fadeIn">
+          <div class="section-header">
+            <h3 class="section-title">Mengapa Memilih Kami?</h3>
+            <p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p>
+          </div>
+          <div class="row">
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.2s">
+              <div class="box">
+                <div class="icon"><i class="fa fa-shield"></i></div>
+                <h4 class="title">100% Confidential</h4>
+                <p class="description">TOUCHE dibangun berdasarkan kepercayaan. TOUCHE ingin klien aman dan nyaman untuk berbagi bersama kami.</p>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.4s">
+              <div class="box">
+                <div class="icon"><i class="fa fa-user-md"></i></div>
+                <h4 class="title">Qualified Team</h4>
+                <p class="description">Klien ditangani hanya oleh profesional berlisensi, baik psikolog, konselor, terapis, maupun trainer.</p>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.6s">
+              <div class="box">
+                <div class="icon"><i class="fa fa-thumbs-up"></i></div>
+                <h4 class="title">Individual Approach</h4>
+                <p class="description">Klien adalah prioritas utama TOUCHE. Perkembangan menuju diri yang optimal adalah tujuan utama TOUCHE berdiri.</p>
+              </div>
+            </div>
+          </div>
+  
+        </div>
+      </section><!-- #services -->
+  
+      <!--========================== Call To Action Section ============================-->
+      <section id="call-to-action">
+        <div class="container wow fadeIn">
+          <div class="row">
+            <div class="col-lg-9 text-center text-lg-left">
+              <h3 class="cta-title">Bergabung dan Berpetualang Bersama Artikel Kami</h3>
+              <p class="cta-text"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="col-lg-3 cta-btn-container text-center">
+              <a class="cta-btn align-middle" href="/blog">BLOG</a>
+            </div>
+          </div>
+  
+        </div>
+      </section>
+  
+      <!--========================== category Section ============================-->
+      <section id="category">
+        <div class="container wow fadeInUp">
+          <div class="section-header">
+            <h3 class="section-title">Blog Kami</h3>
+            <p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p>
+          </div>
+          <div class="row">
+            @if(!empty($category))
+          <div class="row" id="category-wrapper">
+            @foreach ($categories as $category)
+                <div class="col-md-4 col-sm-12 category-item filter-app" >
+                      <a href="">
+                        <img src="{{asset('category_image/'.$category->image)}}" class="image-center">
+                        <div class="details">
+                          <h4>{{$category->name}}</h4>
+                          <span>{{$category->description}}</span>
+                        </div>
+                      </a>
+                </div>
+            @endforeach  
+          </div>
+            @endif
+        </div>
+      </section>
+  
+      <!--========================== Gallery Section ============================-->
+      <section id="contact" style="padding-bottom:85px">
+        <div class="container wow fadeInUp">
+          <div class="section-header">
+            <h3 class="section-title">Galeri</h3>
+            <p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p>
+          </div>
+        </div>
+  
+        <div class="container wow fadeInUp">
+          <div class="row justify-content-center">
+  
+            <div class="col-lg-12 col-md-4">
+              <div class="row">
+                <div class="col-lg-3 col-md-4 col-sm-6 full-img" style="background-image: url({{asset('user/images/gallery/prambanan.png')}})">
+                </div>
+                <div class="col-lg-3 col-md-4 col-sm-6 full-img" style="background-image: url({{asset('user/images/gallery/wisata2.png')}})">
+                </div>
+                <div class="col-lg-3 col-md-4 col-sm-6 full-img" style="background-image: url({{asset('user/images/gallery/wisata3.png')}})">
+                </div>
+                <div class="col-lg-3 col-md-4 col-sm-6 full-img" style="background-image: url({{asset('user/images/gallery/wisata4.png')}})">
+                </div>
+                <div class="col-lg-3 col-md-4 col-sm-6 full-img" style="background-image: url({{asset('user/images/gallery/wisata5.png')}})">
+                </div>
+                <div class="col-lg-3 col-md-4 col-sm-6 full-img" style="background-image: url({{asset('user/images/gallery/wisata6.png')}})">
+                </div>
+                <div class="col-lg-3 col-md-4 col-sm-6 full-img" style="background-image: url({{asset('user/images/gallery/wisata7.png')}})">
+                </div>
+                <div class="col-lg-3 col-md-4 col-sm-6 full-img" style="background-image: url({{asset('user/images/gallery/wisata8.png')}})">
+                </div>
+              </div>
+            </div>
+  
+          </div>
+  
+        </div>
+      </section>
+@endsection
